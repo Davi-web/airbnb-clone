84a7acdfee9ffcd384243f75ecccf653b9201826
Davi-web <jeseung.ha@vanderbilt.edu>
2023-05-23 03:47:34 -0700

    a

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,15 +1,15 @@
 import prisma from "@/app/libs/prismadb";
 
 export interface IListingsParams {
-  userId?: string;
-  guestCount?: number;
-  roomCount?: number;
-  bathroomCount?: number;
-  startDate?: string;
-  endDate?: string;
-  locationValue?: string;
-  category?: string;
-}
+    userId?: string;
+    guestCount?: number;
+    roomCount?: number;
+    bathroomCount?: number;
+    startDate?: string;
+    endDate?: string;
+    locationValue?: string;
+    category?: string;
+  }
 
 export default async function getListings(
   params: IListingsParams
